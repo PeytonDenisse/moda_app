841b256fac3877e9607aed81b76a1f9b94a015ef
Greys2004 <gav77465@lasallebajio.edu.mx>
2025-06-19 23:37:37 -0600

    Cambios en el catalogo_home

--- moda_app/presentation/views/public_view.py
+++ moda_app/presentation/views/public_view.py
@@ -1,41 +1,54 @@
 from django.shortcuts import render, get_object_or_404
+from moda_app.service.inventario_service import InventoryService
 from moda_app.service.product_service import ProductService
 from moda_app.service.category_service import CategoryService
 
 # Servicios
 producto_service = ProductService()
 categoria_service = CategoryService()
-
+inventario_service = InventoryService(  )
 """
     Vista pública
     Muestra todos los productos y las categorías
 """
 
 def catalogo_home(request):
-    
     productos = producto_service.get_all()
     categorias = categoria_service.get_all()
+    inventarios = inventario_service.get_all()
+
+    # Diccionario: producto_id -> cantidad
+    cantidad_por_producto = {inv.product_id: inv.quantity for inv in inventarios}
+
+    # Agregar atributo .cantidad a cada producto
+    for producto in productos:
+        producto.cantidad = cantidad_por_producto.get(producto.id, 0)
+
     return render(request, 'public/catalogo_home.html', {
         'productos': productos,
-        'categorias': categorias
+        'categorias': categorias,
+        'categoria_actual': None
     })
-
-
-
-"""
-    Muestra productos filtrados por categoría.
-"""
 
 def filtrar_por_categoria(request, id):
     productos = [p for p in producto_service.get_all() if p.id_categoria == id]
     categorias = categoria_service.get_all()
+    inventarios = inventario_service.get_all()
+
+    # Diccionario: producto_id -> cantidad
+    cantidad_por_producto = {inv.product_id: inv.quantity for inv in inventarios}
+
+    # Agregar atributo .cantidad a cada producto
+    for producto in productos:
+        producto.cantidad = cantidad_por_producto.get(producto.id, 0)
+
     categoria_actual = next((c for c in categorias if c.id == id), None)
+
     return render(request, 'public/catalogo_home.html', {
         'productos': productos,
         'categorias': categorias,
         'categoria_actual': categoria_actual
     })
-
 
 """
     Muestra el detalle de un producto.
